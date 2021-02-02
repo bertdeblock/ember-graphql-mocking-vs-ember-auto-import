a20987d69ce4ef39a0bd97fe68c4264ba54dd3bd
Bert De Block <bert.deblock@bagaar.be>
2021-02-02 20:02:54 +0100

    chore: set up reproduction

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
+    addons: {
+      blacklist: ['@bagaar/ember-graphql-mocking'],
+    },
   });
 
   /*
